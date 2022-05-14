29ac7510f08694931105278ef680ca8190e82846
Andres Mucino <50926003+andresmucino@users.noreply.github.com>
2022-05-13 22:00:50 -0500

    field for redux

--- packages/react-firebase/src/App.tsx
+++ packages/react-firebase/src/App.tsx
@@ -4,14 +4,11 @@ import { Home, Login, Register } from "./components";
 export const App = () => {
   return (
     <div className="bg-slate-300 h-screen text-blue flex">
-      {/* <AuthProvider> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-
-      {/* </AuthProvider> */}
     </div>
   );
 };
